c0a7ddfabd414ad144cf15d38f587f4489d033e3
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-01-20 13:55:47 -0800

    Changed TODO to DONE

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,7 +3,7 @@
 
   repos.all = [];
 
-  // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  // DONE: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   // repos.requestRepos = function(callback) {
   //   $.ajax({
   //     url: 'https://api.github.com/users/SaulFein/repos' +
@@ -16,16 +16,13 @@
   //     }
   //   }).done(callback);
   // };
-  repos.requestRepos = function(callback){
-    $.ajax({
-      url: '/github/users/SaulFein/repos' +
-            '?per_page=100' + '&sort=updated',
-      type: 'GET',
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
-  };
+
+  repos.requestRepos = function(callback) {
+  $.get("/github/users/SaulFein/repos?per_page=100&sort=updated", function(data) {
+      repos.all = data;
+      // console.log( "Load was performed.");
+  }).done(callback);
+};
 
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
